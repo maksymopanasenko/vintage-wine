a9e2b826dcbd910d100c8cc5112b16c21c96cc0f
maksymopanasenko <maxpanas008@gmail.com>
2023-12-19 22:17:06 +0100

    build app

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -10,7 +10,6 @@ const NoPage = () => {
                     <div className={styles.errorDesc}>Page not found</div>
                 </div>
                 <img src="https://res.cloudinary.com/dhpukux5x/image/upload/v1698006085/fkkpp5a1l20nl7e05im7.png" alt="page not found" />
-                {/* <img src="../../../public/error/error.png" alt="page not found" /> */}
             </div>
         </Container>
     );
